d74bd0dcc91d4cb8405cfb0ae86d2958c170c871
SasakiYuki <wacorggen@gmail.com>
2019-09-25 17:05:34 +0900

    🎨 DesignFix VideoView3つ表示してみた

--- app/src/main/java/com/example/androidhlstest/MainActivity.kt
+++ app/src/main/java/com/example/androidhlstest/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.androidhlstest
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.bottomVideoView
+import kotlinx.android.synthetic.main.activity_main.topVideoView
 import kotlinx.android.synthetic.main.activity_main.videoView
 
 class MainActivity : AppCompatActivity() {
@@ -10,8 +12,12 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
+    topVideoView.setVideoPath(VIDEO_URL)
+    topVideoView.start()
     videoView.setVideoPath(VIDEO_URL)
     videoView.start()
+    bottomVideoView.setVideoPath(VIDEO_URL)
+    bottomVideoView.start()
   }
 
   companion object {
